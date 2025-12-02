043323b21562cae536d41806b33bb919b53cf23c
pcbircka <pcbircka@gmail.com>
2025-12-02 09:56:06 +0300

    Delete src/whisper_service.py

--- src/whisper_service.py
+++
@@ -1,9 +0,0 @@
-import openai
-
-def transcribe_audio(audio_path: str) -> str:
-    with open(audio_path, "rb") as audio:
-        response = openai.audio.transcriptions.create(
-            model="whisper-1",
-            file=audio
-        )
-    return response.text
